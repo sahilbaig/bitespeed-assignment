711b79728e9cb943f1f773f1f42b92b793c6157a
Sahil Baig <sahilbaig.me@gmail.com>
2025-05-30 15:00:17 +0530

    supabse DB added ...connection sucesful

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,24 @@ import express from "express";
 
 const app = express();
 app.use(express.json());
+import dotenv from "dotenv";
+dotenv.config();
+
+import { supabase } from "./utils/supabase/client";
+
+async function testDb() {
+  const { data, error } = await supabase.from("contacts").select("*").limit(1);
+  if (error) {
+    console.error("Supabase query error:", error);
+  } else {
+    console.log("Supabase query success:", data);
+  }
+}
+
+// testDb();
 
 app.get("/", (_, res) => {
+  testDb();
   res.send("Bitespeed server is running");
 });
 
